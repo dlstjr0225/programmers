27d08b1f65f6853149b32257a3eb3fd9795edcd1
dlstjr0225 <drbb0225@gmail.com>
2026-04-24 14:40:50 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT a.TITLE, a.BOARD_ID, b.REPLY_ID, b.WRITER_ID, b.CONTENTS, to_char(b.CREATED_DATE, 'YYYY-MM-DD') as CREATED_DATE
+from USED_GOODS_BOARD a, USED_GOODS_REPLY b
+where 1=1
+and a.BOARD_ID = b.BOARD_ID
+and a.CREATED_DATE >= date '2022-10-01'
+and a.CREATED_DATE < date '2022-11-01'
+order by b.CREATED_DATE, a.TITLE
+;
