3f9fa5e2e5bf49c33020db3776fafc6892da0568
dlstjr0225 <drbb0225@gmail.com>
2026-05-03 17:31:13 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT INGREDIENT_TYPE, sum(TOTAL_ORDER) as TOTAL_ORDER
+from FIRST_HALF a, ICECREAM_INFO b
+where 1=1
+and a.FLAVOR = b.FLAVOR
+group by INGREDIENT_TYPE
+order by TOTAL_ORDER
+;
