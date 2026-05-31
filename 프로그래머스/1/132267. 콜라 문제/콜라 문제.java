89df109e49e5657b8fc7323903a916d9edcda703
dlstjr0225 <drbb0225@gmail.com>
2026-05-31 23:33:42 +0900

    [level 1] Title: 콜라 문제, Time: 0.06 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while (n >= a) {
+            int received = (n / a) * b;
+            int remain = n % a;
+            
+            answer += received;
+            n = received + remain;
+            
+            }
+        
+        return answer;
+    }
+}
